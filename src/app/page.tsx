968dd75140c146eeaa5e682843e4002630a8c79e
Kuiliang Zhang <z1219202167@outlook.com>
2024-02-25 20:59:39 -0800

    feat: add route handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,25 @@
 import TodoForm from "@/components/TodoForm";
 import TodoItem from "@/components/TodoItem";
 import { useStore } from "@/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Home: React.FC = () => {
   const todos = useStore((state) => state.todos);
   const fetchTodos = useStore((state) => state.fetchTodos);
+
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    // Fetch data from the API endpoint
+    fetch("/api/hello")
+      .then((res) => res.json()) // Parse the JSON response
+      .then((data) => {
+        setMessage(data.message); // Set the message in state
+      })
+      .catch((error) => {
+        console.error("Failed to fetch data from '/api/hello':", error);
+        setMessage("Failed to fetch message");
+      });
+  }, []); // The empty array ensures this effect runs once on mount
 
   useEffect(() => {
     fetchTodos();
@@ -22,6 +36,7 @@ const Home: React.FC = () => {
       ) : (
         todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)
       )}
+      <p>Message from API: {message}</p>
     </div>
   );
 };
